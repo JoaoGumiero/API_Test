84777f14532d549fc3316aa0d71885fdea11e484
JoaoGumiero <joaogumiero1410@gmail.com>
2024-01-31 15:27:48 -0300

    add connection with the postgree db

--- main.go
+++ main.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"text/template"
 	"time"
+
+	_ "github.com/lib/pq"
 )
+
+func dbConnection() *sql.DB {
+	connStr := "user=postgres password=123# host=localhost dbname=API_Test sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+type Produto struct {
+	Id              int
+	Nome, Descricao string
+	Preco           float64
+	Quantidade      int
+}
+
+// trazer todos os templates da pasta
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 const monitorings = 2
 const delay = 5
 
 // Precisa de uma função principal para a nossa aplicação, dentro dela sera inserida o código
 func main() {
+	//Subir o servidor
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":800", nil)
+
 	for {
 		showMenu()
 		command := readUserCommand()
@@ -32,6 +59,36 @@ func main() {
 			os.Exit(-1)
 		}
 	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	db := dbConnection()
+	sql, err := db.Query("select * from products")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for sql.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err := sql.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			log.Fatal(err)
+		}
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Quantidade = quantidade
+		p.Preco = preco
+	}
+	produtos = append(produtos, p)
+
+	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
 
 func showMenu() {
