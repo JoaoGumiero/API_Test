ad0f7092841f5349f60b9d65208ce7fcbe6a026d
João Vitor Calvo Gumiero <joaogumiero1410@gmail.com>
2024-02-10 17:36:53 -0300

    Update db.go
    Changing some db connection parameters

--- db/db.go
+++ db/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func DbConnection() *sql.DB {
-	conection := "user=postgres dbname=API_Test password=123# host=localhost sslmode=disable"
+	conection := "user=postgres dbname=[db name] password=[db password] host=localhost sslmode=disable"
 	db, err := sql.Open("postgres", conection)
 	if err != nil {
 		log.Fatal(err)
