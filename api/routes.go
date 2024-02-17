c90b252d095577cc0b7450eb0f96dc902020a6fc
JoaoGumiero <joaogumiero1410@gmail.com>
2024-02-17 18:14:02 -0300

    Adding some improvements to handlers

--- api/routes.go
+++ api/routes.go
@@ -6,13 +6,11 @@ import (
 
 func UploadRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("GET /products", getAllProductsHandler)
 	mux.HandleFunc("/newProduct", NewProductHandler) //handler for only the newproduct page
 	mux.HandleFunc("POST /products", InsertProductHandler)
 	mux.HandleFunc("DELETE /products/{id}", DeleteProductHandler)
 	mux.HandleFunc("PATCH /products/{id}", EditProductHandler)
 	mux.HandleFunc("PUT /products", UpdateProductHandler)
-
 	return mux
 }
